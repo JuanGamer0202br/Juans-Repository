9bcd7108b7e471f5c53eedff7f261701589d54d5
JuanGamer0202br <162936124+JuanGamer0202br@users.noreply.github.com>
2025-06-22 02:11:43 -0300

    Update GURI.vhd

--- codigos codificados/GURI.vhd
+++ codigos codificados/GURI.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity GURI is
 	port (
-		clock , reciever : in std_logic;
+		clock , inicio , reciever : in std_logic;
 		Talk , result : out std_logic
 	);
 end entity;
@@ -21,57 +21,57 @@ architecture uart_generator2 of GURI is
 -- (50000000)/(4800) = ~10417 (valor calculado que um processador de 50Mhz deve usar para se comunicar em 4.8Khz)
 -- Para simulação usar 100
 
-	signal temporizador : integer range 0 to 50000000 := 0; -- vou querer trocar entre os modos da maquina de 1 em 1 segundo
-	signal contagem : integer range 0 to 5208 := 0;
+	signal temporizador : integer range 0 to 50000000 := 0; 
+	signal contagem : integer range 0 to 10417 := 0;
+	signal inicio_old : std_logic;
+	signal i : integer range 0 to 8 := 0; -- o processador vai usar esse sinal para lembrar qual dos 8 bits ele está lendo
+	signal mensagem : std_logic_vector (7 downto 0); -- armazena a sequencia recebida
 
-	signal i : integer range 0 to 8 := 0; -- o processador vai usar esse sinal para lembrar qual dos 8 bits ele está enviando
-	signal mensagem : std_logic_vector (7 downto 0); -- a sequencia 1 a ser enviada é a letra A de hex = 41, ali esta em binario
-
-	TYPE estados IS ( afk , start , sequencia_1 , sequencia_2 , stop ); -- usamos uma maquina de estados, o vdhl não é uma linguagem sequencial, por isso precisamos garantir que partes do código só sejam executadas em um estado especifico
+	TYPE estados IS ( afk , start96 , start48 , lendo , enviando , stop96 , stop48 ); -- usamos uma maquina de estados, o vdhl não é uma linguagem sequencial, por isso precisamos garantir que partes do código só sejam executadas em um estado especifico
 	
 	signal maquina : estados := afk; -- por padrão iniciamos a maquina em away from keyboard
 
 begin
-	process(relogiows)
+	process(clock)
 	begin
-		IF rising_edge(relogiows) THEN
+		IF rising_edge(clock) THEN
 				case maquina is
 					when afk =>
 						
-						Serial <= '1'; -- em quanto nada é executado, a linha de comunicação é mantida ALTA
-						LetsTalk <= '0';
+						result <= '1'; -- em quanto nada é executado, a linha de saída é mantida ALTA
+						Talk <= '0';
 						
-						IF temporizador < 500 then --para simulação usar 5000
-							temporizador <= temporizador + 1;
-						ELSE -- já se passou o tempo 1 segundo
-							maquina <= start;
-							temporizador <= 0;
+						if inicio = '1' and inicio_old = '0' then -- o processador recebeu um pedido de comunicação 
+							i <= 0; -- i = 0 que dizer que ainda não lemos nenhum bit
+							maquina <= start96; -- se preparando para receber o primeiro bit!!!! (esse primeiro bit tem que ser baixo e ele confirma que SIM queremos nos comunicar)
+							contagem <= 0; 
 						end if;
-					when start =>
-		
+						inicio_old <= inicio; -- atualiza inicio pra essa primeira condição não rodar mais não cara ta bom já a gente já tendeu que começou a comunicação
+					when start96 =>
+								-- esse é o start de receber dados, baud aqui é 9600!!!!!!!
 						i <= 0;
-						LetsTalk <= '1'; -- envio uma solicitação de inicio da comunicação
-						Serial <= '0'; -- a linha de comunicação vai para BAIXO indicando que vamos começar a nos comunicar
+						Talk <= '1'; 
+						result <= '0';
+						
 						contagem <= 0;
 
-						if qual_das_duas = 0 then -- escolhe qual sequencia vamos enviar
-								maquina <= sequencia_1;
-								qual_das_duas <= 1;
-						else
-								maquina <= sequencia_2;
-								qual_das_duas <= 0;
-						end if; 
-					when sequencia_1 =>
+						IF CONTAGEM < 25 then --usar metade do baud original
+							CONTAGEM <= CONTAGEM + 1;
+						ELSE -- já se passou o tempo de 1 bit
+							contagem <= 0; 
+							maquina <= lendo; -- agora as coisas ficam interessantes
+						end if;
+					when lendo =>
 						IF CONTAGEM < 50 then --para simulação usar 50
 							CONTAGEM <= CONTAGEM + 1;
 						ELSE -- já se passou o tempo de 1 bit
-							contagem <= 0;
-								
+							contagem <= 0; 
 							if i < 8 then
-								Serial <= seq_1(i); -- o bit "i" da sequencia atual
-								i <= i + 1;	
-							else -- se já se passaram 8 bits, quer dizer que a mensagem foi enviada
-								maquina <= stop; 
+								mensagem(i) <= reciever; -- o bit "i" do bus mensagem recebe o bit atual da entrada
+								i <= i + 1;
+								
+							else -- se já se passaram 8 bits, quer dizer que a mensagem foi recebida
+								maquina <= stop96; 
 							end if;
 						end if; 
 					when sequencia_2 =>
