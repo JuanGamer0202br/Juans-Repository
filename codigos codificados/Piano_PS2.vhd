13ab0ae859fa6fe6c3a2473192453c96a13f5cba
JuanGamer0202br <162936124+JuanGamer0202br@users.noreply.github.com>
2025-07-06 22:51:46 -0300

    Update Piano_PS2.vhd

--- codigos codificados/Piano_PS2.vhd
+++ codigos codificados/Piano_PS2.vhd
@@ -3,7 +3,7 @@ USE IEEE.std_logic_1164.all;
 
 ENTITY Piano_PS2 IS
 	PORT (
-		clk : in std_logic;
+		onboard_clk , keyboard_clk : in std_logic;
 		buzzer : out std_logic
 	);
 	
@@ -12,23 +12,23 @@ END Piano_PS2;
 ARCHITECTURE Main OF Piano_PS2 IS
 
 SIGNAL CLKCOUNT : integer RANGE 0 TO 50000000 := 0;
-SIGNAL SEGUNDO : integer RANGE 0 TO 100000000 := 0;
-SIGNAL BEEP : integer RANGE 0 TO 25000000 := 0;
+SIGNAL BFREQ : integer RANGE 0 TO 50000000 := 5952;
+SIGNAL SEGUNDO : integer RANGE 0 TO 50000000 := 0;
 SIGNAL BUZZ : std_logic := '0';
 
 -- para gerar um tom de 440Hz no buzzer, preciso que o código use o clock da placa
--- portanto faço (50000000 / 440) / 2 = 56818 (dividido por 2 por que o sinal precisa subir e decer)
+-- portanto faço ( ( 50000000 / 440 ) / 2 ) = 56818 (dividido por 2 por que um sinal é formado metade por uma subida e metade uma decida)
 
 BEGIN
 	
-	PROCESS(clk)
+	PROCESS(onboard_clk)
 	BEGIN
 	
 	-- gerador de tom
 	
-	IF RISING_EDGE(clk) THEN
+	IF RISING_EDGE(onboard_clk) THEN
 	
-		IF CLKCOUNT < 5952 THEN
+		IF CLKCOUNT < BFREQ THEN
 		
 			CLKCOUNT <= CLKCOUNT + 1;
 			
@@ -42,25 +42,20 @@ BEGIN
 	
 	END PROCESS;
 	
-	PROCESS(clk)
+	PROCESS(onboard_clk)
 	BEGIN
 	
-	-- controla a saida do buzzer para criar pquenos beeps
+	-- controla a saida do buzzer para liberar o som
 	
-	IF RISING_EDGE(clk) THEN
+	IF RISING_EDGE(onboard_clk) THEN
 	
 		IF SEGUNDO < 50000000 THEN
 	
 			SEGUNDO <= SEGUNDO + 1;
-	
-		ELSIF BEEP < 12500000 THEN
-		
 			buzzer <= BUZZ;
-			BEEP <= BEEP + 1;
 		
 		ELSE
 		
-			BEEP <= 0;
 			SEGUNDO <= 0;
 		
 		END IF;
